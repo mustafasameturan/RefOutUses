5ddb6bbf5f96585925a24113f84c0db01cc8db4f
mustafasameturan <mustafasameturan@hotmail.com>
2021-01-20 21:02:42 +0300

    Add project files.

---
+++ RefOutUses/OutExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace RefOutUses
+{
+    class OutExample
+    {
+
+        static void Main(String[] args)
+        {
+            int variable;
+            UseOut(out variable);
+            Console.WriteLine(variable);
+        }
+        static void UseOut(out int numberOut)
+        {
+            numberOut = 1234;
+        }
+
+    }
+}
+   
---
+++ RefOutUses/RefExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RefOutUses
+{
+    class RefExample
+    {
+        static void Main(string[] args)
+        {
+            int mainValue = 30;
+            Console.WriteLine("Before use RefExample: " + mainValue);
+
+            UseRef useRef = new UseRef();
+            useRef.refExample(5, 6, ref mainValue);
+            Console.WriteLine("After use RefExampe: " + mainValue);
+
+            
+        }
+    }
+
+    class UseRef
+    {
+        public void refExample(int x, int y, ref int i)
+        {
+            i = i + x + y;
+        }
+
+    }
+}
